4b41cc62c57721f7f92108c3c34fe7ba6bb49916
Morad Ichchou <morad.ichchou@gmail.com>
2020-06-17 16:09:49 +0200

    Ajout de getEvaluationsParMot dans evaluation.js

--- controllers/evaluation.js
+++ controllers/evaluation.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 
 
 // @desc        Requête qui affiche les sous-catégories pour une catégorie donnée
-// @route       POST /api/v1/bilans/categorie
+// @route       POST /api/v1/evaluation/sous-categorie
 // @access      Public
 
 // SELECT P.nomCategorieP FROM categorie_p AS P WHERE P.nomCategorieG= :categG
@@ -28,12 +28,41 @@ exports.getSousCategories = async (req, res, next) => {
     }
 };
 
+
+// @desc        Requête qui affiche les évaluations pour une sous-catégorie donnée
+// @route       POST /api/v1/evaluation/evaluation
+// @access      Public
+
 exports.getEvaluations = async (req, res, next) => {
     try {
         const categ = req.body.categ;
 
         const result = await DB.query("SELECT E.nomEvaluation FROM evaluation E WHERE E.nomCategorieP = :categP", {
             replacements: {categP: categ },
+            raw: true,
+            type: Sequelize.QueryTypes.SELECT
+        });
+
+        res.status(200).json({ success: true, data: result });
+
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+// @desc        Requête qui affiche les évaluations pour un mot ou lettre donnée
+// @route       POST /api/v1/evaluation/evaluationParMot
+// @access      Public
+
+// L'usage de CONCAT() dans la requête est néccessaire car une fois la requête appelée, la variable  :mot est tranqformée en requête séparée des symboles '%'
+
+exports.getEvaluationsParMot = async (req, res, next) => {
+    try {
+        const mot = req.body.mot;
+
+        const result = await DB.query("SELECT E.nomEvaluation FROM evaluation E WHERE E.nomEvaluation LIKE CONCAT('%',:mot,'%') ", {
+            replacements: {mot: mot },
             raw: true,
             type: Sequelize.QueryTypes.SELECT
         });
--- routes/evaluation.js
+++ routes/evaluation.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getSousCategories, getEvaluations} = require('../controllers/evaluation');
+const { getSousCategories, getEvaluations, getEvaluationsParMot} = require('../controllers/evaluation');
 
 //mergeParams allows crossRouting
 const router = express.Router({ mergeParams: true });
 
 router.route('/sous-categorie').post(getSousCategories);
 router.route('/evaluation').post(getEvaluations);
+router.route('/evaluationParMot').post(getEvaluationsParMot);
 
 module.exports = router;
